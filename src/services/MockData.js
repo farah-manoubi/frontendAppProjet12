93fd0e3ff68445b5da61c7faf7d53c40e3f58564
farah <farah.manoubi@hotmail.com>
2023-10-28 14:54:53 +0200

    suppression console.log

--- src/services/MockData.js
+++ src/services/MockData.js
@@ -1,7 +1,6 @@
 export const getUserIdentityMock = async (userId) =>{
     const url = `../../${userId}/userApi.json`;
     const response = await fetch(url);
-    console.log(response)
     return await response.json();
 }
 
